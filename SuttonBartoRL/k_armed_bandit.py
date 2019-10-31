b1e6e1ab4500e6aedd44a65802b012c9b08591f7
CiaranWelsh <c.welsh2@newcastle.ac.uk>
2019-10-31 21:51:33 +0000

    added subdirectory for Sutton and Barto book. Found repo for k armed bandit problem. Want to solve this problem before moving on. It was built on an earlier version of k-arm bandits. Might be good to submit a PR when you are done fixing it

---
+++ SuttonBartoRL/k_armed_bandit.py
@@ -0,0 +1,32 @@
+import os
+
+from gym_bandits.bandit import *
+
+
+
+
+
+env = gym.make("BanditTwoArmedDeterministicFixed-v0")
+#
+# for i in range(10):
+#     # observation = env.reset()
+#     for j in range(10):
+#         action = env.action_space.sample()
+#         observation, reward, done, info = env.step(action)
+#         print(observation)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
